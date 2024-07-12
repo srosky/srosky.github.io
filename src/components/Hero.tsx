64cffa68a86f5843a23f29fd868e553ab6994456
simon-o3h <simon@oooh.tv>
2024-07-11 20:01:40 -0700

    Updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 
 import {Button} from '@/components/Button'
@@ -5,7 +6,9 @@ import {Container} from '@/components/Container'
 import logo3 from '@/images/logo3-min-resize.png'
 
 export function Hero() {
+
     return (
+
         <Container className="pb-16 pt-5 text-center lg:pt-10">
             <div className="flex justify-center items-center">
                 <Image
@@ -26,7 +29,12 @@ export function Hero() {
                 Bespoke software studio. Building custom solutions for your business needs.
             </p>
             <div className="mt-10 flex justify-center gap-x-6">
-                <Button href="mailto:contact@bitbotlabs.com">Get Started</Button>
+                <Button id="mlink" href="#" onMouseOver={()=>{
+                    // Get the mLink element
+                    const mLink = document.getElementById("mlink");
+                    // Set the href attribute to the mLink
+                    mLink?.setAttribute("href", "mailto:&#x63;&#x6f;&#x6e;&#x74;&#x61;&#x63;&#x74;&#x40;&#x62;&#x69;&#x74;&#x62;&#x6f;&#x74;&#x6c;&#x61;&#x62;&#x73;&#x2e;&#x63;&#x6f;&#x6d;");
+                }}>Get Started</Button>
 
             </div>
 
